088231db99b02d21ce4897c7cc4b2a510828d2ee
clay hendrix <clayhendrix5@gmail.com>
2022-10-22 11:35:14 -0500

    adding styles to rest-rant

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-
+//Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
+//express settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
+app.use(express.static('public'))
+//controllers and routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -15,5 +16,5 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
   res.render('error404')
 })
-
+//Listen for connections
 app.listen(process.env.PORT)
