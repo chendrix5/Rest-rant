f2277978fdb3141e584c1bea9f158594228e6397
clay hendrix <clayhendrix5@gmail.com>
2022-10-26 17:56:49 -0500

    added comment section

--- Views/places/show.jsx
+++ Views/places/show.jsx
@@ -1,11 +1,19 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{data.place.name}</h1>
+            <h2> {data.place.pic}</h2>
+            <h3> {data.place.city}, {data.place.state} </h3>
+            <h4> {data.place.cuisines} </h4>
+            <h2> RATINGS </h2>
+            <p> No ratings yet</p>
+            <h2> Comments </h2>
+            <p> No comments yet</p>
+            
           </main>
         </Def>
     )
