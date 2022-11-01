9e2d47b2cdf61d607f5b19d2a84c3e29fd140392
clay hendrix <clayhendrix5@gmail.com>
2022-10-31 20:11:10 -0500

    new stuff

--- Views/places/show.jsx
+++ Views/places/show.jsx
@@ -6,8 +6,9 @@ function show (data) {
         <Def>
           <main>
             <h1>{data.place.name}</h1>
-            <img src={data.place.pic} alt=""/>
-            <h3> {data.place.city}, {data.place.state} </h3>
+            <img src={data.place.pic} alt={data.place.name}/>
+            <h3> Located in {data.place.city}, {data.place.state} </h3>
+            <h3> {data.place.showEstablished()}</h3>
             <h4> {data.place.cuisines} </h4>
             <h2> RATINGS </h2>
             <p> No ratings yet</p>
