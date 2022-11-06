7e9efb295efa5452a7d780fce57e9dbb630f9de7
clay hendrix <clayhendrix5@gmail.com>
2022-11-05 20:32:13 -0500

    star rating shows

--- Views/places/show.jsx
+++ Views/places/show.jsx
@@ -7,7 +7,21 @@ function show (data) {
       No comments yet!
     </h3>
   )
+    let rating = (
+      <h3 className= "inactive">
+        Not yet rated
+      </h3>
+    )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = sumRatings / data.place.comments.length
+    rating = (
+      <h3>
+       {Math.round (averageRating)} stars
+      </h3>
+    )
     comments = data.place.comments.map (c =>{
       return (
         <div className="border">
@@ -30,6 +44,7 @@ function show (data) {
             <h3> {data.place.showEstablished()}</h3>
             <h4> {data.place.cuisines} </h4>
             <h2> RATINGS </h2>
+            {rating}
             <p> No ratings yet</p>
             <h2> Comments </h2>
             {comments}
